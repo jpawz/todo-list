933eab0547d197f8a9a5f405b696233e429122c3
Pawel J <jpawz@outlook.com>
2023-02-26 17:30:52 +0100

    Add Task class

---
+++ src/modules/Task.js
@@ -0,0 +1,22 @@
+export default class Task {
+  constructor(name, dueDate) {
+    this.name = name;
+    this.dueDate = dueDate;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setDueDate(dueDate) {
+    this.dueDate = dueDate;
+  }
+
+  getDueDate() {
+    return this.dueDate;
+  }
+}
